5f961c15502d49a99ffe9c370415519d3a12e40f
Sergio Ostanioni <ostanioni@gmail.com>
2021-02-23 05:38:33 +0700

    last changes

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,88 +5,76 @@ import java.util.Map;
 import java.util.zip.DataFormatException;
 
 public class Calculator {
-    static Map<String,Integer> RomeNumbers = new HashMap<String,Integer>(){{
-        put(   "I",  1);
-        put(  "II",  2);
-        put( "III",  3);
-        put(  "IV",  4);
-        put(   "V",  5);
-        put(  "VI",  6);
-        put( "VII",  7);
-        put("VIII",  8);
-        put(  "IX",  9);
-        put(   "X",  10);
+    static Map<String, String> mapRomanArabNumbers = new HashMap<String,String>(){{
+        put(   "I",  "1");
+        put(  "II",  "2");
+        put( "III",  "3");
+        put(  "IV",  "4");
+        put(   "V",  "5");
+        put(  "VI",  "6");
+        put( "VII",  "7");
+        put("VIII",  "8");
+        put(  "IX",  "9");
+        put(   "X",  "10");
     }};
-    private static int toArabicNumber(String romeNumber){
-        return RomeNumbers.get(romeNumber);
+    private static int toArabNumber(String romanNumber){
+        return Integer.parseInt( mapRomanArabNumbers.get(romanNumber) );
     }
-    private static boolean isValidNumbers(String[] numbers) throws DataFormatException {
-        if (numbers.length == 2){
-            numbers[0] = numbers[0].trim();
-            numbers[1] = numbers[1].trim();
-        } else {
-            return false;
-        }
-        if ( ( isRomeNumber(numbers[0]) & isArabicNumber(numbers[1]) ) |
-                ( isArabicNumber(numbers[0]) & isRomeNumber(numbers[1]) ) ){
+    private static boolean isValidInput(String[] inputData) throws DataFormatException {
+        if ( ( isRomanNumber( inputData[0] ) & isArabNumber(inputData[1]) ) |
+             ( isArabNumber(inputData[0]) & isRomanNumber(inputData[1]) ) ){
             throw new DataFormatException("Неверный формат данных: использованы римские и арабские формы записи!");
         }
-        boolean isValid =
-                 ( isRomeNumber(numbers[0]) & isRomeNumber(numbers[1]) ) |
-                 ( isArabicNumber(numbers[0]) & isArabicNumber(numbers[1]) );
+        boolean isValid = ( isRomanNumber(inputData[0]) & isRomanNumber(inputData[1]) ) |
+                          ( isArabNumber(inputData[0]) & isArabNumber(inputData[1]) );
         return isValid;
     }
-    private static boolean isRomeNumber(String str){
-        return RomeNumbers.containsKey(str);
+    private static boolean isRomanNumber(String str){
+        return mapRomanArabNumbers.containsKey(str);
     }
-    private static boolean isArabicNumber(String str){
-        return RomeNumbers.containsValue(str);
+    private static boolean isArabNumber(String str){
+        return mapRomanArabNumbers.containsValue(str);
     }
-    private static int[] splitLine(String line) throws DataFormatException, NumberFormatException {
-        String[] stringNumbers = line.split("[+]|[-]|[*]|[/]");
+    private static int[] splitLine(String line) throws DataFormatException, NumberFormatException, NullPointerException {
+        String[] inputData = line.split("[+]|[-]|[*]|[/]");
+        if ( inputData.length == 2 ) {
+            inputData[0] = inputData[0].trim();
+            inputData[1] = inputData[1].trim();
+        } else {
+            throw new DataFormatException("Неверный формат данных");
+        }
         try {
-            if( stringNumbers.length == 1 ){
-                throw new DataFormatException("Неверный формат данных...");
-            }
-            if ( isValidNumbers(stringNumbers) ){
-                if( isRomeNumber(stringNumbers[0]) ) {
-                    int numbers[] = { toArabicNumber(stringNumbers[0]), toArabicNumber(stringNumbers[1])};
-                    return numbers;
+            if ( isValidInput(inputData) ){
+                if( isRomanNumber( inputData[0]) ) {
+                    int[] inputNumbers = { toArabNumber( inputData[0] ), toArabNumber( inputData[1] ) };
+                    return inputNumbers;
                 }
-                int numbers[] = { Integer.parseInt(stringNumbers[0]), Integer.parseInt(stringNumbers[1])};
-                return numbers;
+                int[] inputNumbers = { Integer.parseInt( inputData[0] ), Integer.parseInt( inputData[1] ) };
+                return inputNumbers;
             }
-
         } catch(DataFormatException e){
             StdOut.println(e.getMessage());
+
         }
-        int numbers[] = { Integer.parseInt(stringNumbers[0]), Integer.parseInt(stringNumbers[1])};
-        return numbers;
+        return null;
     }
     private static char getOperation(String line){
-        if ( line.indexOf("+") > 0 ) {
-            return '+';
-        }
-        if ( line.indexOf("-") > 0 ) {
-            return '-';
-        }
-        if ( line.indexOf("*") > 0 ) {
-            return '*';
-        }
+        if ( line.indexOf("+") > 0 ) return '+';
+        if ( line.indexOf("-") > 0 ) return '-';
+        if ( line.indexOf("*") > 0 ) return '*';
         return '/';
     }
     public static void main(String[] args){
         while ( true ){
-
             StdOut.println("Введите данные, которые необходимо вычислить (для выхода введите \"exit\"):");
             String line = StdIn.readLine().trim();
 
             if ( line.equals("exit") ) break;
 
             try {
-                int[] numbers = splitLine(line);
-                int firstNumber = numbers[0],
-                        secondNumber = numbers[1];
+                int[] inputNumbers = splitLine(line);
+                int firstNumber = inputNumbers[0],
+                    secondNumber = inputNumbers[1];
                 char operation = getOperation(line);
 
                 switch (operation){
@@ -99,10 +87,14 @@ public class Calculator {
                 StdOut.println(e.getMessage());
             } catch (NumberFormatException e){
                 StdOut.println(e.getMessage());
+            } catch (NullPointerException e){
+                StdOut.println(e.getMessage());
             } finally {
-                // if ( line.equals("exit") ) break;
+                /* StdOut.println("Введите данные в формате 4 + 6, VII - V, 9 * 10. " +
+                        "\nИспользовать римские или арабские числа от 1 до 10." +
+                        "\nНе использовать римские и арабские цифры вместе.");
+                 */
             }
         }
-
     }
 }
